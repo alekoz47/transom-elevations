20cf51fd034b2efc0c8c5fc9906b2ef12c5d0765
Alex Koziol <akoziol21@webb.edu>
2020-07-15 17:30:40 -0400

    fixed 1500 frames

--- src/slice_video.py
+++ src/slice_video.py
@@ -3,11 +3,21 @@ import cv2
 
 vidcap = cv2.VideoCapture("../videos/TR5-R1.94A1V.mp4")
 success,image = vidcap.read()
+
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+duration = frame_count / fps
+rate = duration / 1500
+
+print(rate * 1000)
+
 count = 0
 success = True
 while success:
-    cv2.imwrite("../images/frame%d.jpg" % count, image)   #save frame #count as jpeg
+    if count % 100 == 0:
+        print(count)
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * rate * 1000))
+    cv2.imwrite("../images/frame%d.jpg" % count, image)
     success,image = vidcap.read()
-    print("Read a new frame: ", success)
     count += 1
 vidcap.release()
