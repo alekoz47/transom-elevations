3c168e5bd87e89d32ec44ba1e010fc1139c1fbee
Alex Koziol <akoziol21@webb.edu>
2020-07-17 13:58:46 -0400

    added video test of mask

---
+++ src/mask_loop.py
@@ -0,0 +1,67 @@
+
+import cv2
+import numpy as np
+
+def find_mask(image_name):
+    print(image_name)
+    img = cv2.imread(image_name)
+        
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #np.savetxt("test1.csv", img.split()[0], delimiter=',')
+    
+    # project image to vertical plane
+    #   this is based on first frame of TR5-R1.94A1V
+    #   based on camera position, this shouldn't change between runs
+    initial_rect = np.float32([[732, 480],
+                               [1151, 483],
+                               [1144, 576],
+                               [741, 572]])
+    final_rect = np.float32([[732, 480],
+                             [1151, 480],
+                             [1151, 576],
+                             [732, 576]])
+    matrix = cv2.getPerspectiveTransform(initial_rect, final_rect)
+    proj = cv2.warpPerspective(img, matrix, (1920, 1080))
+    
+    # hide everything but stern (based on yellow)
+    hsv = cv2.cvtColor(proj, cv2.COLOR_RGB2HSV)
+    lower_range = np.array([20,100,50])
+    upper_range = np.array([60,242,215])
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+    
+    # crop image to only include stern + wave
+    crop = mask[150:700, 500:1350]
+    
+    ######## Recognize Grid ########
+    
+    gray = crop
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+    
+    contours, hierarchy = cv2.findContours(thresh,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    best_cnt = contours[0]
+        
+    max_area = 0
+    c = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 1000:
+            if area > max_area:
+                max_area = area
+                best_cnt = i
+        c += 1
+    
+    mask = np.zeros((gray.shape), np.uint8)
+    cv2.drawContours(mask, [best_cnt], 0, 255, -1)
+    cv2.drawContours(mask, [best_cnt], 0, 0, 2)
+    
+    out = np.zeros_like(gray)
+    out[mask == 255] = gray[mask == 255]
+    
+    return mask
+
+for i in range(1500):
+    mask = find_mask("../images/frame" + str(i) + ".jpg")
+    cv2.imwrite("../images/testing/frame" + str(i) + ".jpg", mask)
---
+++ src/unslice_video.py
@@ -0,0 +1,30 @@
+
+import cv2
+import os
+from os.path import isfile, join
+
+path_in = "../images/testing/"
+path_out = "../videos/testing/video.mp4"
+fps = 60
+
+frame_array = []
+files = [f for f in os.listdir(path_in) if isfile(join(path_in, f))]
+
+#sort file names properly
+files.sort(key = lambda x: x[5:-4])
+files.sort()
+
+for i in range(len(files)):
+    filename = path_in + files[i]
+    img = cv2.imread(filename)
+    h, w, layers = img.shape
+    size = (w, h)
+    frame_array.append(img)
+
+out = cv2.VideoWriter(path_out,
+                      cv2.VideoWriter_fourcc(*'DIVX'),
+                      fps,
+                      size)
+for i in range(len(frame_array)):
+    out.write(frame_array[i])
+out.release()
