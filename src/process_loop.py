d8d4d1b4911173d567bfb6d33c97bcaae87bf20b
Alex Koziol <akoziol21@webb.edu>
2020-07-28 17:13:56 -0400

    added initial morphology code

--- src/process_loop.py
+++ src/process_loop.py
@@ -91,6 +91,31 @@ def elevations(transom_contour, waterline_contour):
     wave_heights = [-h * scale for h in unscaled_wave_heights]
     return wave_heights
 
+def apply_morphology(masked_image):
+    # smooth the image with alternative closing and opening
+    # with an enlarging kernel
+    morph = masked_image.copy()
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
+    morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    
+    # take morphological gradient
+    gradient_image = cv2.morphologyEx(morph, cv2.MORPH_GRADIENT, kernel)
+    
+    # split the gradient image into channels
+    image_channels = np.split(np.asarray(gradient_image), 3, axis=2)
+    channel_height, channel_width, _ = image_channels[0].shape
+    
+    # apply Otsu threshold to each channel
+    for i in range(0, 3):
+        _, image_channels[i] = cv2.threshold(~image_channels[i], 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)
+        image_channels[i] = np.reshape(image_channels[i], newshape=(channel_height, channel_width, 1))
+    
+    # merge the channels
+    image_channels = np.concatenate((image_channels[0], image_channels[1], image_channels[2]), axis=2)
+    return image_channels
+
 def test_mask(frame):
     image_name = "../images/frame%d.jpg" % frame
     img = cv2.imread(image_name)
@@ -118,7 +143,7 @@ def get_elevations(data_path):
         
         prj = corrected_perspective(img)
         tsm = masked_image(prj, np.array([26,150,130]), np.array([30,255,215]))
-        transom = largest_contour(tsm)
+        transom = largest_contour(apply_morphology(tsm))
         wtl = masked_image(prj, np.array([30,204,105]), np.array([40,255,224]))
         waterline = largest_contour(wtl)
         heights = elevations(transom, waterline)
