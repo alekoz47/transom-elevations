03cd5a30966dc7019ab3751ef1f9f7749eb122cc
Alex Koziol <akoziol21@webb.edu>
2020-07-28 14:50:11 -0400

    fixed printout in video split

--- src/analyze_video.py
+++ src/analyze_video.py
@@ -15,8 +15,7 @@ def slice_video(video_path):
     count = 0
     success = True
     while success:
-        if count % 100 == 0:
-            print(count)
+        print(count)
         vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * rate * 1000))
         cv2.imwrite("../images/frame%d.jpg" % count, image)
         success,image = vidcap.read()
@@ -24,8 +23,10 @@ def slice_video(video_path):
     vidcap.release()
 
 
-for filename in os.listdir("../videos/"):
-    video_path = filename
-    slice_video(video_path)
-    data_path = video_path.replace("videos", "data")
-    get_elevations(data_path)
+for filename in os.listdir("../videos/2016-06-29_T5"):
+    if filename != "Thumbs.db":
+        video_path = "../videos/2016-06-29_T5/" + filename
+        print(video_path)
+        slice_video(video_path)
+        data_path = video_path.replace("videos", "data")
+        get_elevations(data_path)
