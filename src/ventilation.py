3de73a8cd4eda6927309ffa0201a04b5dceb2071
Alex Koziol <akoziol21@webb.edu>
2020-08-06 15:30:45 -0400

    added ventilation min/max/steady (incomplete)

--- src/ventilation.py
+++ src/ventilation.py
@@ -3,12 +3,12 @@ import os
 import csv
 import re
 import math
+import numpy as np
 
 def get_ventilation(data_path):
     """Find average ventilation factor for given run"""
     
-    # differentiate between T1 and T5 hulls
-    static_draft = 0
+    # test for T1 hull
     if data_path.find("T1") > 0:
         static_draft = 0.029
     else:
@@ -17,19 +17,43 @@ def get_ventilation(data_path):
     with open(data_path, 'r') as data:
         data_reader = csv.reader(data, delimiter=',')
         steady_data = list(data_reader)[400:1100]
+    steady_data = [map(float, row) for row in steady_data]
     
-    sum_elevations = [0 for i in range(7)]
-    for row in steady_data:
-        num_row = [float(i) for i in row]
-        sum_elevations = map(sum, zip(sum_elevations, num_row))
-    elevations = [elevation / len(steady_data) for elevation in sum_elevations]
+    # test for steady run
+    if data_path.find("A1VS") > 0:
+        elevations = map(get_elevation_averages_steady, steady_data)
+    else:
+        elevations = map(get_elevation_averages_unsteady, steady_data)
         
     drafts = [static_draft + elevation for elevation in elevations]
-    ventilations = [(static_draft - draft) / static_draft for draft in drafts]
+    drafts = map(lambda el: el + static_draft, elevations)
+    ventilations = [vf(static_draft, draft) for draft in drafts]
     return ventilations
 
+def get_elevation_averages_steady(steady_data):
+    """Find average ventilation factor for steady runs"""
+    
+    quantiles = np.quantile(steady_data, 3)
+    average_ventilations = np.median(quantiles[1])
+    
+    return [average_ventilations, 0]
+    
+def get_elevation_averages_unsteady(steady_data):
+    """Find min and max ventilation factors for unsteady runs"""
+    
+    quantiles = np.quantile(steady_data, 4)
+    min_ventilations = np.median(quantiles[0])
+    max_ventilations = np.median(quantiles[3])
+    
+    return [min_ventilations, max_ventilations]
+
+def vf(static_draft, draft):
+    return (static_draft - draft) / static_draft
 
 
+
+get_ventilation("../data/2016-06-29_T5/TR5-R1.94A1V.csv")
+"""
 # scan all T5 runs
 for filename in os.listdir("../data/2016-06-29_T5"):
     if filename != "Thumbs.db":
@@ -59,4 +83,4 @@ for filename in os.listdir("../data/2016-06-27_T1"):
             row = [fn] + ventilations
             write.writerows([row])
         print("Data %s analysis complete." % filename)
-
+"""
