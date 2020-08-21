d70aacd92c0b3ea4763d818323adaa535ecca73a
Alex Koziol <akoziol21@webb.edu>
2020-08-21 16:29:46 -0400

    fixed steady range for consistency

--- src/fourier.py
+++ src/fourier.py
@@ -9,7 +9,7 @@ def get_elevations(data_path, buttock):
     
     with open(data_path, 'r') as data:
         data_reader = csv.reader(data, delimiter=',')
-        steady_data = list(data_reader)[700:1050]
+        steady_data = list(data_reader)[700:1000]
     steady_data = [map(float, row) for row in steady_data]
     steady_data = [list(row) for row in steady_data]
     
@@ -18,7 +18,7 @@ def get_elevations(data_path, buttock):
 
 
 
-elevations = get_elevations("../data/2016-06-27_T1/T1-R1.94A1V_1.csv", 3)
+elevations = get_elevations("../data/2016-06-27_T1/T1-R3.00A1V.csv", 3)
 x = np.array(elevations)
 y = fft(x)
 y = list(map(abs, y))
@@ -32,8 +32,8 @@ y0[incident_max_index] = 0
 x0 = ifft(y0)
 x0 = list(map(abs, x0))
 
-plt.subplot(221),plt.plot(x),plt.ylim(-0.02,0.01),plt.xlim(0,400)
-plt.subplot(222),plt.plot(x0),plt.ylim(-0.02,0.01),plt.xlim(0,400)
+plt.subplot(221),plt.plot(x),plt.ylim(-0.02,0.01),plt.xlim(0,300)
+plt.subplot(222),plt.plot(x0),plt.ylim(-0.02,0.01),plt.xlim(0,300)
 plt.subplot(223),plt.plot(y),plt.ylim(0,1),plt.xlim(0,60)
 plt.subplot(224),plt.plot(y0),plt.ylim(0,1),plt.xlim(0,60)
 plt.show()
